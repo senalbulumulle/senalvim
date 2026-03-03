db6ce0db5a507fdeffae014b44341ad5075500e0
senalbulumulle <senal.bulumulle@gmail.com>
2026-03-03 06:41:28 -0800

    no need

--- shell-scripts/install--package--hoscartio-cursor-nvim-plugin.nvim.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/hoscarcito/cursor-nvim-plugin ~/.local/share/nvim/site/pack/plugins/start/cursor-nvim-plugin
-
-
--- shell-scripts/install-package--plenary.nvim.sh
+++
@@ -1,3 +0,0 @@
-## install-package--plenary.nvim.sh
-
-git clone https://github.com/nvim-lua/plenary.nvim ~/.local/share/nvim/site/pack/plugins/start/plenary.nvim
