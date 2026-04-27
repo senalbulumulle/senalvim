9509743f2550ad68ff42ea5300058ede9c4906ef
senalbulumulle <senal.bulumulle@gmail.com>
2026-04-27 19:38:02 +0530

    preparing to make the config system with lua api for neovim

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 // main.c
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
+#define SENAL_VIM_CONFIG_PATH "config"
 
 
 int main(int argc, char *argv[]) 
 {
+        // allows the ability to init and create the config folder
+        // and then parse it out with lua
+        FILE *file = fopen(SENAL_VIM_CONFIG_PATH, "w");
         return 0;
 }
